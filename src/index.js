e79aa5980d32ef635a855a460151900bd046f474
Yauhen Klimovich <webrubin1987@gmail.com>
2023-01-16 14:33:37 +0300

    feat: towelSort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
+module.exports = function towelSort(matrix) {
 
-// You should implement your task here.
+    let myArray = [];
+    let secretArray = ['RSS', 'SCHOOL'];
+    let js = ['javaScript'];
 
-module.exports = function towelSort (matrix) {
-  return [];
+    for (let item = 0; item < arguments.length; item++) {
+        myArray[item] = arguments[item];
+    }
+    console.log(myArray);
+
+    if (myArray.length === js.length-1) {
+        return [];
+    }
+
+    let resultValueMatrix = matrix.reduce((accumulator, currentValue, index) => {
+        if (index % secretArray.length) {
+            currentValue.reverse();
+        }
+        return [...accumulator, ...currentValue];
+    }, []);
+
+    return resultValueMatrix;
 }
